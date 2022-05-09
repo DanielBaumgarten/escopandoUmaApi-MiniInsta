f214de752a8f16fd76df5f6d9f3ca0139f884123
DanielBaumgarten <86086435+DanielBaumgarten@users.noreply.github.com>
2022-05-09 19:12:19 -0300

    inclusao de rotas

--- src/rotas.js
+++ src/rotas.js
@@ -1,3 +1,12 @@
 const express = require('express');
+const login = require('./controladores/login');
+const verificaLogin = require('./filtros/verificaLogin');
+
 
 const rotas = express()
+
+rotas.post('/login', login.login);
+
+rotas.use(verificaLogin)
+
+module.exports = rotas
