e5bea6cb11bbcf0f15704888ac0f1bc7c8ab81e3
DanielBaumgarten <86086435+DanielBaumgarten@users.noreply.github.com>
2022-05-13 17:33:07 -0300

    atualizando e refatorando rotas

--- src/rotas.js
+++ src/rotas.js
@@ -1,18 +1,21 @@
 const express = require('express');
-const usuarios = require('./controladores/usuarios');
-const login = require('./controladores/login');
+const { cadastrarUsuario, obterPerfil, atualizarPerfil } = require('./controladores/usuarios');
+const { login } = require('./controladores/login');
 const verificaLogin = require('./filtros/verificaLogin');
+const { novaPostagem } = require('./controladores/postagens');
 
 
 const rotas = express()
 
-rotas.post('/cadastro', usuarios.cadastrarUsuario);
+rotas.post('/cadastro', cadastrarUsuario);
 
-rotas.post('/login', login.login);
+rotas.post('/login', login);
 
 rotas.use(verificaLogin);
 
-rotas.get('/perfil', usuarios.obterPerfil);
-rotas.put('/perfil', usuarios.atualizarPerfil)
+rotas.get('/perfil', obterPerfil);
+rotas.put('/perfil', atualizarPerfil);
+
+rotas.post('/postagens', novaPostagem);
 
 module.exports = rotas
