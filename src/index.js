0d4968c02f495e7e79255b92a34743d143fd21a1
DanielBaumgarten <86086435+DanielBaumgarten@users.noreply.github.com>
2022-07-04 10:49:40 -0300

    <feat> : <implementando a utilização do cors>

--- src/index.js
+++ src/index.js
@@ -6,7 +6,13 @@ const cors = require('cors');
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000"),
+    res.header("Acess-Control-Allow-Methods", 'GET,PUT,POST')
+    app.use(cors());
+    next();
+})
 app.use(rotas);
 
 app.listen(3000);
